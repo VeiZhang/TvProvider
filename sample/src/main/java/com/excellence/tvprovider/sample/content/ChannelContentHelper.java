bafc6bd2ec8c5e210ed229808cf8e0f2f777d30b
Tiimor <tiimor@qq.com>
2022-12-12 12:03:32 +0800

    更新Channel信息

--- sample/src/main/java/com/excellence/tvprovider/sample/content/ChannelContentHelper.java
+++ sample/src/main/java/com/excellence/tvprovider/sample/content/ChannelContentHelper.java
@@ -1,12 +1,13 @@
 package com.excellence.tvprovider.sample.content;
 
 import android.app.Activity;
+import android.content.ComponentName;
 import android.content.ContentUris;
 import android.content.Context;
 import android.content.Intent;
+import android.database.Cursor;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
-import android.media.tv.TvContract;
 import android.net.Uri;
 import android.os.AsyncTask;
 import android.util.Log;
@@ -74,10 +75,10 @@ public class ChannelContentHelper {
 
         @Override
         protected Long doInBackground(ChannelContents... params) {
-            return addChannel(mActivity, params[0]);
+            return addChannelAction(mActivity, params[0]);
         }
 
-        private Long addChannel(Context context, ChannelContents channelContents) {
+        private Long addChannelAction(Context context, ChannelContents channelContents) {
             String inputId = createInputId(context);
             // 1.创建Channel通道
 
@@ -85,15 +86,37 @@ public class ChannelContentHelper {
                     .setDisplayName(channelContents.getCategory())
                     .setDescription(channelContents.getDescription())
                     .setType(TvContractCompat.Channels.TYPE_PREVIEW)
-                    // 通道输入ID，不知道是啥，可为空
+                    /**
+                     * 通道输入ID，作为查询的标识，如果为空则下面更新Channel的操作会出现异常：NOT NULL constraint failed: channels.input_id
+                     */
                     .setInputId(inputId)
                     // 1.1点击MainScreen左边上的分类，跳转
                     .setAppLinkIntent(context.getPackageManager().getLaunchIntentForPackage(context.getPackageName()))
                     .setInternalProviderId(channelContents.getChannelId())
                     .build();
 
-            Uri channelUri = context.getContentResolver().insert(TvContractCompat.Channels.CONTENT_URI,
-                    channel.toContentValues());
+            Uri channelUri = null;
+            if (checkChannel(context, channelContents)) {
+                channelUri = TvContractCompat.buildChannelUri(channelContents.getId());
+                /**
+                 * 已存在时，需要更新Channel的操作
+                 *
+                 * Selection not allowed for content://android.media.tv/channel 异常查询：https://stackoverflow.com/questions/31377712/live-channels-query-returns-security-exception
+                 * 可以更换TvContractCompat.Channels.CONTENT_URI为content://android.media.tv/channel/150
+                 */
+                int result = context.getContentResolver().update(
+                        Uri.parse(String.format("content://android.media.tv/channel/%s", channelContents.getId())),
+                        channel.toContentValues(), null, null);
+                if (result < 1) {
+                    Log.e(TAG, "addChannel: update failed");
+                } else {
+                    Log.i(TAG, "addChannelAction: update success");
+                }
+            } else {
+                channelUri = context.getContentResolver().insert(TvContractCompat.Channels.CONTENT_URI,
+                        channel.toContentValues());
+            }
+
             if (channelUri == null || channelUri.equals(Uri.EMPTY)) {
                 Log.e(TAG, "addChannel: failed");
                 return null;
@@ -108,8 +131,25 @@ public class ChannelContentHelper {
              * 3.在MainScreen上左侧显示的图标
              */
             createChannelLogo(context, channelId, R.drawable.logo);
+            addProgramAction(context, channelId, channelContents.getVideos());
 
-            List<Videos> videosList = channelContents.getVideos();
+            return channelId;
+        }
+
+        private void addProgramAction(Context context, long channelId, List<Videos> videosList) {
+//            int result = context.getContentResolver().delete(TvContractCompat.buildChannelUri(channelId), null, null);
+//            if (result < 1) {
+//                Log.e(TAG, "addProgramAction: clear failed ");
+//            }
+
+//            Cursor cursor = context.getContentResolver().query(TvContractCompat.buildChannelUri(channelId), null, null, null);
+//            if (cursor != null && cursor.moveToNext()) {
+//                deletePrograms(channelId, videosList);
+            addPrograms(context, channelId, videosList);
+//            }
+        }
+
+        private void addPrograms(Context context, long channelId, List<Videos> videosList) {
             for (Videos clip : videosList) {
                 String clipId = clip.getVideoId();
 
@@ -139,8 +179,34 @@ public class ChannelContentHelper {
                     long programId = ContentUris.parseId(programUri);
                 }
             }
+        }
 
-            return channelId;
+        private void deletePrograms(long channelId, List<Videos> videosList) {
+
+        }
+
+        /**
+         * 检查Channel是否已添加
+         */
+        private boolean checkChannel(Context context, ChannelContents channelContents) {
+            Cursor cursor = context.getContentResolver().query(TvContractCompat.Channels.CONTENT_URI, null, null, null);
+            if (cursor != null && cursor.moveToFirst()) {
+                do {
+                    Channel channel = Channel.fromCursor(cursor);
+                    /**
+                     * 根据 数据的Id标识 判别是否已添加
+                     */
+                    if (channelContents.getChannelId().equals(channel.getInternalProviderId())) {
+                        /**
+                         * 由于通道Id是空的，需要赋值
+                         */
+                        channelContents.publishId(channel.getId());
+                        return true;
+                    }
+
+                } while (cursor.moveToNext());
+            }
+            return false;
         }
 
         private void createChannelLogo(Context context, long channelId, int drawableId) {
@@ -149,7 +215,7 @@ public class ChannelContentHelper {
         }
 
         private String createInputId(Context context) {
-            return null;
+            return TvContractCompat.buildInputId(new ComponentName(context.getPackageName(), ""));
         }
 
         @Override
@@ -162,18 +228,20 @@ public class ChannelContentHelper {
              * 2.执行了Intent，则显示提示框，然后确认添加到MainScreen
              */
 
-            try {
-                if (channelId == null) {
-                    Log.e(TAG, "onPostExecute: failed to add channel on home screen");
-                    return;
-                }
+//            try {
+//                if (channelId == null) {
+//                    Log.e(TAG, "onPostExecute: failed to add channel on home screen");
+//                    return;
+//                }
+//
+//                Intent intent = new Intent(TvContract.ACTION_REQUEST_CHANNEL_BROWSABLE);
+//                intent.putExtra(TvContractCompat.EXTRA_CHANNEL_ID, channelId);
+//                mActivity.startActivityForResult(intent, ADD_CHANNEL_REQUEST);
+//            } catch (Exception e) {
+//                e.printStackTrace();
+//            }
 
-                Intent intent = new Intent(TvContract.ACTION_REQUEST_CHANNEL_BROWSABLE);
-                intent.putExtra(TvContractCompat.EXTRA_CHANNEL_ID, channelId);
-                mActivity.startActivityForResult(intent, ADD_CHANNEL_REQUEST);
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
+            TvContractCompat.requestChannelBrowsable(mActivity, channelId);
         }
     }
 
